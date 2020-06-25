52a33fb4ebbfd4e68c4415f5f4888df2b8eb23ba
H409131 <66553203+H409131@users.noreply.github.com>
2020-06-25 15:36:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+var Paper, ground;
+var bin;
+var b;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload() {
+  Paper1 = loadImage("unnamed.png");
+  bin1 = loadImage("images.png");
+  b = loadImage("c.jpg");
+}
+
+function setup() {
+  createCanvas(800, 700);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  package = createSprite(400, 400, 200, 500);
+  package.addImage(b);
+
+  packageSprite = createSprite(200, 620, 10, 10);
+  packageSprite.addImage(Paper1);
+  packageSprite.scale = 0.2;
+  // packageSprite.visible = true;
+
+  packageSprite1 = createSprite(600, 590, 10, 10);
+  packageSprite1.addImage(bin1);
+  packageSprite1.scale = 0.7;
+
+  //Create a Ground
+  groundSprite = createSprite(width / 2, height - 35, width, 10);
+  groundSprite.shapeColor = color(255);
+
+  Paper = Bodies.circle(width / 2, 200, 5, {
+    restitution: 1.5,
+    isStatic: true
+  });
+
+  World.add(world, Paper);
+  // World.add(world, Dustbin);
+
+  //Create a Ground
+  ground = Bodies.rectangle(width / 2, 650, width, 10, { isStatic: true });
+  World.add(world, ground);
+
+  Engine.run(engine);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(b);
+
+  if (keyCode === UP_ARROW) {
+    packageSprite.x = packageSprite.x + 0.5;
+    packageSprite.y = packageSprite.y + -0.5;
+  }
+
+  if (keyCode === DOWN_ARROW) {
+    packageSprite.y = packageSprite.y + 0.5;
+  }
+
+  // if (packageSprite.istouching(packageSprite1)) {
+  // packageSprite.visible = false;
+  //}
+
+  drawSprites();
+}
